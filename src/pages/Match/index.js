c3fa79f7ff1021b917e995958c1fd191c3dc55b4
ayushneekhar <a.neekhar01@gmail.com>
2022-01-22 02:10:37 +0530

    Initial modal design

--- src/pages/Match/index.js
+++ src/pages/Match/index.js
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import React from "react";
+import TeamCard from "../../components/TeamModal/TeamModal";
 
 export default function Match() {
-  const params = useParams();
-  return <h1>Match {params.matchId}</h1>;
+	const params = useParams();
+	return <TeamCard match={params.matchId} />;
 }
