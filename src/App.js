5016ea6c18d25f8d3ac08f320c609ae88f0433b6
ayushneekhar <a.neekhar01@gmail.com>
2022-01-22 00:04:46 +0530

    Deleted extra files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "antd/dist/antd.css";
+import TournamentCard from "./components/TournamentCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<TournamentCard />
+		</div>
+	);
 }
 
 export default App;
