aca623b27558957aeee8cf140b4c9794d5c6d4f0
Kaustubh Debnath <kaustubh.debnath10@gmail.com>
2022-01-22 17:06:38 +0530

    hostmatch page

---
+++ src/pages/HostMatch/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+const HostPage = () => {
+  return (
+    <div>
+      <h1>HOST MATCH</h1>
+    </div>
+  );
+};
+
+export default HostMatch;
