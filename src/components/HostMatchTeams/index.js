b0cf4693cb189d57a2aca54d3dac3d1cea1f6711
Sumit Prakash <sumit0311@gmail.com>
2022-01-22 21:10:08 +0530

    HostMatch

---
+++ src/components/HostMatchTeams/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import "./style.css";
+import { Input, InputNumber, Button } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
+
+const HostMatchTeams = (props) => {
+  function onChange(value) {
+    console.log("changed", value);
+  }
+  return (
+    <div>
+      <div className="teamsCard">
+        <h1 className="formHead">Configure Team #{props.id}</h1>
+        <p className="formHeadText">
+          Fill team members if you have some already, and list other positions
+          vacant for others to participate!
+        </p>
+        <p className="text1">
+          We allow a maximum of two teams on individually organised matches
+        </p>
+        <div className="gap">
+          <p className="text1">Enter Team Name</p>
+          <Input className="inputField" />
+        </div>
+        <div className="gap">
+          <div className="side">
+            <p className="text1">Number of Players</p>
+          </div>
+          <div className="side">
+            <InputNumber
+              min={1}
+              max={11}
+              onChange={onChange}
+              className="inputNumber1"
+            />
+          </div>
+        </div>
+        <div className="gap">
+          <p className="text1">Type of Players and Numbers</p>
+          <div className="side">
+            <Input className="inputField1" />
+          </div>
+          <div className="side">
+            <InputNumber
+              min={1}
+              max={11}
+              onChange={onChange}
+              className="inputNumber"
+            />
+          </div>
+          <div className="side">
+            <Input className="inputField1" />
+          </div>
+          <div className="side">
+            <InputNumber
+              min={1}
+              max={11}
+              onChange={onChange}
+              className="inputNumber"
+            />
+          </div>
+          <div className="side">
+            <Input className="inputField1" />
+          </div>
+          <div className="side">
+            <InputNumber
+              min={1}
+              max={11}
+              onChange={onChange}
+              className="inputNumber"
+            />
+          </div>
+          <div className="side">
+            <Input className="inputField1" />
+          </div>
+          <div className="side">
+            <InputNumber
+              min={1}
+              max={11}
+              onChange={onChange}
+              className="inputNumber"
+            />
+          </div>
+        </div>
+        <div className="next">
+          <div className="side">
+            <Button
+              shape="circle"
+              icon={<ArrowRightOutlined />}
+              className="nextArrow"
+            ></Button>
+          </div>
+          <div className="side">
+            <p className="text nextText">Finalize</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HostMatchTeams;
--- src/pages/HostMatch/index.js
+++ src/pages/HostMatch/index.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
+import HostMatchForm from "../../components/HostMatchForm/index.js";
+import HostMatchFinal from "../../components/HostMatchFinal/index.js";
+import HostMatchTeams from "../../components/HostMatchTeams/index.js";
 
 const HostMatch = () => {
   return (
     <div>
-      <h1>HOST MATCH</h1>
+      <h1 className="headingTitle">HOST A MATCH</h1>
+      <p className="headingText">
+        Are you an individual/club/organisation wanting to list your in person
+        sports event on Powerplay just by filling up this form!
+      </p>
+      {/* <HostMatchForm /> */}
+      {/* <HostMatchFinal /> */}
+      <HostMatchTeams id={1} />
     </div>
   );
 };
