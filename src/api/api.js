d20002ee40e9415ab50178315d75c06c4c5a143c
ayushneekhar <a.neekhar01@gmail.com>
2022-01-23 04:24:54 +0530

    Fixed player count

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/",
+	baseURL: "/api/",
 });
 
 export const fetchMatches = async () => {
-  const response = await api.get("/match");
-  return response.data;
+	const response = await api.get("/match");
+	return response.data;
 };
 
 export const fetchMatchById = async (id) => {
-  const response = await api.get(`/match/${id}`);
-  return response.data;
+	const response = await api.get(`/match/${id}`);
+	return response.data;
 };
 
 export const fetchTeamById = async (id) => {
-  const response = await api.get(`/teams/${id}`);
-  return response.data;
+	const response = await api.get(`/teams/${id}`);
+	return response.data;
+};
+export const fetchPlayerById = async (id) => {
+	const response = await api.get(`/players/${id}`);
+	return response.data;
 };
--- src/components/TeamModal/TeamModal.js
+++ src/components/TeamModal/TeamModal.js
@@ -3,107 +3,99 @@ import "antd/dist/antd.css";
 import { useState, useEffect } from "react";
 import "./style.css";
 import { IoMdArrowRoundBack } from "react-icons/io";
-import { fetchTeamById } from "../../api/api";
-
-const data = [
-  {
-    title: "Aman Kumar",
-    role: "Bowler",
-  },
-  {
-    title: "Ayush Neekhar",
-    role: "Bowler",
-  },
-  {
-    title: "Sumit Prakash",
-    role: "Jokster",
-  },
-  {
-    title: "Kaustubh Debnath",
-    role: "Bowler (c)",
-  },
-  {
-    title: "S C Ashwin",
-    role: "Carrier",
-  },
-];
+import { fetchTeamById, fetchPlayerById } from "../../api/api";
 
 const TeamModal = (props) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [team, setTeam] = useState({});
-  const [value, setValue] = useState("Aman Kumar");
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [team, setTeam] = useState({});
+	const [player, setPlayer] = useState({});
+	const [value, setValue] = useState("Aman Kumar");
 
-  useEffect(() => {
-    const fetchTeam = async () => {
-      const teamDetails = await fetchTeamById(props.text);
-      if (teamDetails) {
-        var matches = teamDetails.title.match(/\b(\w)/g);
-        var acronym = matches.join("");
-        setTeam({ ...teamDetails, initial: acronym });
-      }
-    };
+	useEffect(() => {
+		const fetchTeam = async () => {
+			const teamDetails = await fetchTeamById(props.text);
+			if (teamDetails) {
+				var matches = teamDetails.title.match(/\b(\w)/g);
+				var acronym = matches.join("");
+				setTeam({ ...teamDetails, initial: acronym });
+				let arr = [];
+				teamDetails.players.forEach(async (element) => {
+					const playerDetails = await fetchPlayerById(element);
+					if (playerDetails) {
+						arr.push(playerDetails);
+						setPlayer({ arr });
+					}
+					// fetchPlayer();
+				});
+				for (let i = 0; i < 11 - teamDetails.players.length; i++) {
+					arr.push({ id: null, name: `Player-${i + 1}` });
+				}
+			}
+		};
+		fetchTeam();
+	}, [props.text]);
 
-    fetchTeam();
-  }, [props.text]);
+	const toggleRadio = (e) => {
+		setValue(e.target.value);
+	};
 
-  const toggleRadio = (e) => {
-    setValue(e.target.value);
-  };
+	const showModal = () => {
+		setIsModalVisible(true);
+	};
 
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+	const handlePayment = () => {
+		setIsModalVisible(false);
+	};
 
-  const handlePayment = () => {
-    setIsModalVisible(false);
-  };
-
-  return (
-    <div>
-      <Button className="team-title" type="link" onClick={showModal}>
-        {team.initial}
-      </Button>
-      <Modal
-        closeIcon={<IoMdArrowRoundBack />}
-        keyboard={true}
-        centered={true}
-        title="Configure Team #1"
-        visible={isModalVisible}
-        onCancel={handleCancel}
-        footer={[
-          <Button
-            key="payment"
-            onClick={handlePayment}
-            style={{ width: "100%", borderRadius: "10px" }}
-          >
-            Continue to Payment
-          </Button>,
-        ]}
-      >
-        <Card style={{ width: "100%" }}>
-          <div className="teamDiv">{team.title}</div>
-          <List
-            itemLayout="horizontal"
-            dataSource={data}
-            renderItem={(item) => (
-              <List.Item>
-                <List.Item.Meta title={item.title} description={item.role} />
-                <Radio
-                  value={item.title}
-                  checked={value === item.title}
-                  onClick={toggleRadio}
-                />
-              </List.Item>
-            )}
-          />
-        </Card>
-      </Modal>
-    </div>
-  );
+	return (
+		<div>
+			<Button className="team-title" type="link" onClick={showModal}>
+				{team.initial}
+				{/* {console.log(team.capacity)} */}
+			</Button>
+			<Modal
+				closeIcon={<IoMdArrowRoundBack />}
+				keyboard={true}
+				centered={true}
+				title="Configure Team #1"
+				visible={isModalVisible}
+				onCancel={handleCancel}
+				footer={[
+					<Button
+						key="payment"
+						onClick={handlePayment}
+						style={{ width: "100%", borderRadius: "10px" }}
+					>
+						Continue to Payment
+					</Button>,
+				]}
+			>
+				<Card style={{ width: "100%" }}>
+					<div className="teamDiv">{team.title}</div>
+					<List
+						itemLayout="horizontal"
+						dataSource={player.arr}
+						renderItem={(item) => (
+							<List.Item>
+								<List.Item.Meta title={item.name} />
+								{item.id === null && (
+									<Radio
+										value={item.name}
+										checked={value === item.name}
+										onClick={toggleRadio}
+									/>
+								)}
+							</List.Item>
+						)}
+					/>
+				</Card>
+			</Modal>
+		</div>
+	);
 };
 
 export default TeamModal;
