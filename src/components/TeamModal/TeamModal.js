c81c3026bc07431fd5966aad0652d713f88c6018
ayushneekhar <a.neekhar01@gmail.com>
2022-01-22 20:32:43 +0530

    Modal fixes

--- src/components/TeamModal/TeamModal.js
+++ src/components/TeamModal/TeamModal.js
@@ -1,19 +1,29 @@
-import { Card, List, Modal, Button } from "antd";
+import { Card, List, Modal, Button, Radio } from "antd";
 import "antd/dist/antd.css";
 import { useState } from "react";
+import "./style.css";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const data = [
 	{
-		title: "Ant Design Title 1",
+		title: "Aman Kumar",
+		role: "Bowler",
 	},
 	{
-		title: "Ant Design Title 2",
+		title: "Ayush Neekhar",
+		role: "Bowler",
 	},
 	{
-		title: "Ant Design Title 3",
+		title: "Sumit Prakash",
+		role: "Jokster",
 	},
 	{
-		title: "Ant Design Title 4",
+		title: "Kaustubh Debnath",
+		role: "Bowler (c)",
+	},
+	{
+		title: "S C Ashwin",
+		role: "Carrier",
 	},
 ];
 
@@ -24,41 +34,50 @@ const TeamModal = (props) => {
 		setIsModalVisible(true);
 	};
 
-	const handleOk = () => {
-		setIsModalVisible(false);
-	};
-
 	const handleCancel = () => {
 		setIsModalVisible(false);
 	};
 
+	const handlePayment = () => {
+		setIsModalVisible(false);
+	};
+
 	return (
-		<>
+		<div>
 			Match: {props.match}
 			<Button type='text' onClick={showModal}>
 				View Team
 			</Button>
 			<Modal
-				title='Current Team'
+				closeIcon={<IoMdArrowRoundBack />}
+				keyboard={true}
+				centered={true}
+				title='Configure Team #1'
 				visible={isModalVisible}
-				onOk={handleOk}
-				onCancel={handleCancel}>
+				onCancel={handleCancel}
+				footer={[
+					<Button
+						key='payment'
+						onClick={handlePayment}
+						style={{ width: "100%", borderRadius: "10px" }}>
+						Continue to Payment
+					</Button>,
+				]}>
 				<Card style={{ width: "100%" }}>
+					<div className='teamDiv'>{props.match}</div>
 					<List
 						itemLayout='horizontal'
 						dataSource={data}
 						renderItem={(item) => (
 							<List.Item>
-								<List.Item.Meta
-									title={<a href='https://ant.design'> {item.title} </a>}
-									description='Ant Design, a design language for background applications, is refined by Ant UED Team'
-								/>
+								<List.Item.Meta title={item.title} description={item.role} />
+								<Radio />
 							</List.Item>
 						)}
 					/>
 				</Card>
 			</Modal>
-		</>
+		</div>
 	);
 };
 
