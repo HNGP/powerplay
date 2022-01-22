68e9ced7d62d4cdd8aa917fe31fa4125745142e8
ashwin-sekar <ashwinsekar14@gmail.com>
2022-01-22 15:41:24 +0530

    added scripts for heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+const express = require("express");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const path = require("path");
 const port = process.env.PORT || 3200;
 
+const app = express();
 server.use(middlewares);
 server.use(router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 server.listen(port);
