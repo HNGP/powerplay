24d3c30d6109c935aa31885dffc4817f43015cd1
Kaustubh Debnath <kaustubh.debnath10@gmail.com>
2022-01-22 17:11:30 +0530

    added to router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from "./components/Navbar";
 import Landing from "./pages/Landing";
 import Match from "./pages/Match";
 import Matches from "./pages/Matches";
+import HostMatch from "./pages/HostMatch";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/matches" element={<Matches />} />
           <Route path="/matches/:matchId" element={<Match />} />
+          <Route path="/hostMatch" element={<HostMatch />} />
           <Route
             path="*"
             element={
